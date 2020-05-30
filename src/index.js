3718c4c856c87efb84d4f7493f7d4c700ff1e37c
Arjun Adhikari <=reasonworld12345@gmail.com>
2020-05-30 19:55:16 +0545

    content on body

--- src/index.js
+++ src/index.js
@@ -7,19 +7,19 @@ const port = process.env.PORT || 4000;
 app.get('/',(req,res)=>{
 res.sendFile(container+'/index.html')
 })
-let count = 0
-io.on('connection',(socket)=>{
-    console.log('Connection Created')
-socket.emit('countUpdate',count)
-socket.on('clicked',()=>{
-    count++
-    io.emit('countUpdate',count)
-})
-socket.on('disconnect',()=>{
-    console.log('Disconnected')
-})
+let clients = 0;
+io.on('connection', (socket)=>{
+   clients++;
+   io.emit('broadcast',{ description: clients + ' clients connected!'});
+   socket.on('disconnect',  ()=>{
+      clients--;
+      io.emit('broadcast',{ description: clients + ' clients connected!'});
+   });
+});
 
-})
+http.listen(3000, function() {
+   console.log('listening on localhost:3000');
+});
 
 server.listen(port,()=>{
     console.log("Server Running on port "+port)
